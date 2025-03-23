aad2160c471f96abd2246322112cc95d959d0825
jhamredahl <jhamredahl@gmail.com>
2025-03-23 13:39:39 +0100

    Optimisation of both prime sieve and trial division, yet still not fast enough.

--- nonprimefactors.cs
+++ nonprimefactors.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using Kattis.IO;
@@ -6,51 +7,68 @@ using Kattis.IO;
 
 namespace NonPrimeFactors {
     internal class Program {
-
-        static bool[] primeSieve = PrimeSieve(2000000);
         static void Main(string[] args) {
 
-            Scanner scanner = new Scanner();
-            int throwAway = scanner.NextInt();
-            while (scanner.HasNext()) {
-                using (BufferedStdoutWriter writer = new BufferedStdoutWriter()) {                
-                    writer.WriteLine(NPFCalc(scanner.NextInt()));
+            bool[] primeSieve = PrimeSieve(2000000);
+            Tokenizer tokenizer = new Tokenizer();            
+            int calls = toInt(tokenizer.Next());
+
+            using (BufferedStdoutWriter writer = new BufferedStdoutWriter()) {
+                while (tokenizer.HasNext()) {
+                    writer.Write(NPFCalc(toInt(tokenizer.Next())) + "\n");
                 }
+                writer.Flush();
             }
-        }
 
-        static int NPFCalc (int n) {
+            int NPFCalc(int n) {
 
-            int primeFactorCalc = 1;
-            int primeUnique = 0;
-            int primeTemp = 0;
+                int primeFactorCalc = 1;
+                int primeUnique = 0;
+                int primeTemp = 0;
+                int sqrtN = Convert.ToInt32(Math.Sqrt(n));
 
-            for (int p = 2; p <= n; p++) {
-                primeTemp = 0;
-                if (!primeSieve[p]) {
-                    if (n % p == 0) {
-                        primeUnique++;
-                        while (n % p == 0) {
-                            primeTemp++;
-                            n /= p;
+                for (int p = 2; p <= sqrtN; p++) {
+                    primeTemp = 0;
+                    if (!primeSieve[p]) {
+                        if (n % p == 0) {
+                            primeUnique++;
+                            while (n % p == 0) {
+                                primeTemp++;
+                                n /= p;
+                            }
+                            primeFactorCalc *= (primeTemp + 1);
                         }
-                        primeFactorCalc *= (primeTemp + 1);                    
-                    }                    
-                }
-            }           
-            return primeFactorCalc - primeUnique;
-        }
-
-        static bool[] PrimeSieve(int i) {
-            bool[] sieve = new bool[i];
-            for (int j = 2; j < i; j++) {
-                if (!sieve[j]) {
-                    for (int k = j * 2; k < i; k += j) {
-                        sieve[k] = true;
                     }
                 }
+                if (n >= sqrtN) {
+                    primeFactorCalc *= 2;
+                    primeUnique++;
+                }
+                return primeFactorCalc - primeUnique;
             }
-            return sieve;
+
+            bool[] PrimeSieve(int n) {
+                int len = Convert.ToInt32(Math.Sqrt(n)) + 1;
+                int sqrtLen = Convert.ToInt32(Math.Sqrt(len));
+                bool[] sieve = new bool[len];
+
+                for (int i = 2; i <= sqrtLen; i++) {
+                    if (!sieve[i]) {
+                        for (int j = i * i; j < len; j += i) {
+                            sieve[j] = true;
+                        }
+                    }
+                }
+                return sieve;
+            }
+
+            int toInt(string s) {
+                int n = 0;
+                for (int i = 0; i < s.Length; i++) {
+                    n = n * 10 + (s[i] - '0');
+                }
+                return n;
+            }
         }
     }
 }
